c10dc799f25653dc48624b43f24005c21cd02bf0
Henry Blanchette <henry@monkeyface.com>
2025-03-03 21:27:40 -0500

    createServer_simplified and getRequestBody are working

--- src/Tlsp/Backend/Server.purs
+++ src/Tlsp/Backend/Server.purs
@@ -2,32 +2,30 @@ module Tlsp.Backend.Server where
 
 import Prelude
 
-import Control.Promise (Promise, fromAff)
+import Control.Promise (Promise, fromAff, toAffE)
+import Data.Either.Nested (type (\/))
 import Data.Maybe (Maybe(..))
 import Data.String (Pattern(..))
 import Data.String as String
 import Data.Tuple.Nested (type (/\), (/\))
 import Effect (Effect)
-import Effect.Aff (Aff)
+import Effect.Aff (Aff, throwError)
 import Effect.Class (liftEffect)
 import Effect.Class.Console as Console
 import Node.Encoding (Encoding(..))
-import Node.EventEmitter (on_, once_)
+import Node.EventEmitter (once_)
 import Node.FS.Sync as FS
-import Node.HTTP (createServer)
 import Node.HTTP.IncomingMessage as IncomingMessage
 import Node.HTTP.OutgoingMessage as OutgoingMessage
-import Node.HTTP.Server (requestH, toNetServer)
+import Node.HTTP.Server (toNetServer)
 import Node.HTTP.ServerResponse as ServerResonse
 import Node.HTTP.ServerResponse as ServerResponse
-import Node.HTTP.Types (HttpServer, IMServer, IncomingMessage, OutgoingMessage, ServerResponse)
+import Node.HTTP.Types (HttpServer, IMServer, IncomingMessage, ServerResponse)
 import Node.Net.Server (listenTcp, listeningH)
 import Node.Path as Path
-import Node.Stream (closeH, dataH, dataHEither, dataHStr, endH, readableH)
 import Node.Stream as Stream
 import Tlsp.Backend.Spec (Backend)
 import Tlsp.Common (dist_dirpath)
-import Unsafe.Coerce (unsafeCoerce)
 
 host = "localhost"
 port = 8100
@@ -37,12 +35,8 @@ url_base = "http://" <> host <> ":" <> show port
 make_main :: Backend -> Effect Unit
 make_main backend = do
   Console.log $ "backend: " <> backend.name
-  -- server <- createServer
 
   server <- createServer_simplified \in_msg res -> do
-    -- inspect_in_msg in_msg
-    -- let in_stream = in_msg # IncomingMessage.toReadable
-
     -- response is successful unless marked as failure later
     ServerResonse.setStatusCode success_StatusCode res # liftEffect
 
@@ -56,12 +50,9 @@ make_main backend = do
     case method of
       "POST" -> case url of
         "/tlsp/test" -> do
-          -- in_str <- Stream.readString in_stream UTF8
-          -- Console.logShow { in_str }
-          -- body
-          -- in_buf <- Stream.read in_stream
-          -- Console.logShow { in_buf: in_buf # map (const "<buf>") }
-          Stream.writeString out_stream UTF8 "hello from back to front " # void # liftEffect
+          in_str <- getRequestBody in_msg
+          Console.logShow { in_str }
+          Stream.writeString out_stream UTF8 "hello from back to front" # void # liftEffect
         _ -> pure unit
       "GET" -> case url of
         _ | Just (filepath /\ contentType) <- from_url_to_local_file url -> do
@@ -77,51 +68,6 @@ make_main backend = do
       _ -> ServerResonse.setStatusCode unimplemented_StatusCode res # liftEffect
 
     Stream.end out_stream # liftEffect
-
-  -- server
-  --   # on_ requestH \in_msg res -> do
-  --       -- inspect_in_msg in_msg
-  --       let in_stream = in_msg # IncomingMessage.toReadable
-  --       in_stream # on_ readableH do Console.log "in_stream: on readable"
-  --       in_stream # on_ dataH \_ -> Console.log "in_stream: on dataH" -- X
-  --       in_stream # on_ dataHStr \_ -> Console.log "in_stream: on dataHStr" -- X
-  --       in_stream # on_ dataHEither \_ -> Console.log "in_stream: on dataHEither" -- X
-  --       in_stream # on_ endH do Console.log "in_stream: on end"
-  --       in_stream # on_ closeH do Console.log "in_stream: on close"
-
-  --       -- response is successful unless marked as failure later
-  --       ServerResonse.setStatusCode success_StatusCode res
-
-  --       let out_msg = res # ServerResponse.toOutgoingMessage
-  --       let out_stream = out_msg # OutgoingMessage.toWriteable
-  --       let url = in_msg # IncomingMessage.url
-  --       let method = in_msg # IncomingMessage.method
-
-  --       Console.logShow { url, method }
-
-  --       case method of
-  --         "POST" -> case url of
-  --           "/tlsp/test" -> do
-  --             -- in_str <- Stream.readString in_stream UTF8
-  --             -- Console.logShow { in_str }
-  --             -- body
-  --             -- in_buf <- Stream.read in_stream
-  --             -- Console.logShow { in_buf: in_buf # map (const "<buf>") }
-  --             void $ Stream.writeString out_stream UTF8 "hello from back to front "
-  --           _ -> pure unit
-  --         "GET" -> case url of
-  --           _ | Just (filepath /\ contentType) <- from_url_to_local_file url -> do
-  --             FS.exists filepath >>= case _ of
-  --               false -> do
-  --                 Console.log $ "not found: " <> filepath
-  --                 ServerResonse.setStatusCode notFound_StatusCode res
-  --               true -> do
-  --                 out_msg # OutgoingMessage.setHeader "Content-Type" contentType
-  --                 content <- FS.readTextFile UTF8 filepath
-  --                 void $ Stream.writeString out_stream UTF8 content
-  --           _ -> ServerResonse.setStatusCode unimplemented_StatusCode res
-  --         _ -> ServerResonse.setStatusCode unimplemented_StatusCode res
-  --       Stream.end out_stream
 
   server
     # toNetServer
@@ -155,4 +101,12 @@ createServer_simplified
   -> Effect HttpServer
 createServer_simplified requestListener =
   createServer_simplified_ (\req res -> requestListener req res # fromAff)
+
+foreign import getRequestBody_
+  :: { ok :: String -> String \/ String, error :: String -> String \/ String }
+  -> IncomingMessage IMServer
+  -> Effect (Promise (String \/ String))
+
+getRequestBody :: IncomingMessage IMServer -> Aff (String \/ String)
+getRequestBody req = getRequestBody_ { ok: pure, error: throwError } req # toAffE
 
